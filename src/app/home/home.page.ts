c5c9f26822806147c63007fd70b94a1607e11700
martha.softwaredeveloper <martha.softwaredeveloper@gmail.com>
2019-10-13 00:10:35 -0400

    Adding reorder component and update array's data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public items: string[];
+
+  constructor(){
+    this.items = [
+      'Bruce',
+      'Ivy',
+      'Harvey', 
+      'Jim',
+      'Lucius',
+      'Victor'
+    ];
+  }
+
+  reorderItems(event){
+    console.log(event);
+    let itemToMove = this.items.splice(event.detail.from, 1)[0];
+    this.items.splice(event.detail.to, 0, itemToMove);
+    event.detail.complete();
+  }
+
+  save(){
+    console.log(this.items);
+  }
 }
